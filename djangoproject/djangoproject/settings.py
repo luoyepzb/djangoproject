66fbdd1e64b5fa51a6a03c1c70dcc6b186446178
pan <168040364@qq.com>
2020-12-10 13:49:54 +0800

    bug

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3o4-vz=&qz-w5tf43z_hdzadwsvtl-y!5#karv5!666y7v5p&p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
